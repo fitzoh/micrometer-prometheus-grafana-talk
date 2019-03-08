13dd870efeee0dd7c068dea5f19e964a53360a46
Andrew Fitzgerald <andrewcfitzgerald@gmail.com>
2019-03-07 23:17:22 -0500

    fixes, logging, automatic load

--- src/main/java/com/github/fitzoh/monitoring/RandomEndpoint.kt
+++ src/main/java/com/github/fitzoh/monitoring/RandomEndpoint.kt
@@ -4,17 +4,28 @@ import io.micrometer.core.instrument.MeterRegistry
 import net.logstash.logback.argument.StructuredArguments
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.InitializingBean
+import org.springframework.boot.context.event.ApplicationReadyEvent
+import org.springframework.context.ApplicationListener
+import org.springframework.context.annotation.Bean
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.reactive.function.client.WebClient
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Flux.*
 import reactor.core.publisher.Mono
+import reactor.core.scheduler.Schedulers
 import java.time.Duration
 import java.time.Instant
 import java.util.*
+import java.util.concurrent.CompletableFuture
 import java.util.concurrent.atomic.AtomicLong
 import java.util.concurrent.atomic.AtomicReference
+import java.util.concurrent.locks.ReentrantLock
+import java.util.concurrent.locks.ReentrantReadWriteLock
 
 
 data class ChaosEvent(val start: Instant = Instant.EPOCH, val end: Instant = Instant.EPOCH) {
@@ -26,9 +37,10 @@ data class ChaosEvent(val start: Instant = Instant.EPOCH, val end: Instant = Ins
 private fun Random.nextInt(lower: Int, upper: Int) = lower + this.nextInt(upper - lower)
 
 
-class ChaosState {
+class ChaosState(val type: String) {
 
     private val event = AtomicReference(ChaosEvent())
+    private val lock = ReentrantLock()
 
     fun isEventActive(): Boolean {
         val now = Instant.now()
@@ -40,12 +52,16 @@ class ChaosState {
     }
 
     private fun scheduleNextEvent(now: Instant) {
-        val secondsUntilNextEvent = random.nextInt(10, 45)
-        val start = now.plusSeconds(secondsUntilNextEvent.toLong())
-        val duration = random.nextInt(5, 15)
-        val end = start.plusSeconds(duration.toLong())
-        event.set(ChaosEvent(start, end))
-        log.info("next chaos event scheduled: {} {}", StructuredArguments.keyValue("startsIn", secondsUntilNextEvent), StructuredArguments.keyValue("duration", duration))
+        if (lock.tryLock()) {
+            val secondsUntilNextEvent = random.nextInt(60, 120)
+            val start = now.plusSeconds(secondsUntilNextEvent.toLong())
+            val duration = random.nextInt(20, 40)
+            val end = start.plusSeconds(duration.toLong())
+            event.set(ChaosEvent(start, end))
+            Mono.delay(Duration.ofSeconds(secondsUntilNextEvent.toLong()))
+                    .subscribe { log.info("{} chaos event started", type) }
+            lock.unlock()
+        }
     }
 
     companion object {
@@ -56,22 +72,22 @@ class ChaosState {
 
 @Component
 class LatencyGenerator {
-    private val chaos = ChaosState()
+    private val chaos = ChaosState("latency")
     fun next(): Int {
         val roll = random.nextFloat()
 
         return if (chaos.isEventActive()) {
-            when {
-                roll < .5 -> fast()
-                roll < .9 -> medium()
-                else -> slow()
-            }
-        } else {
-            when {
+            (200 + 1.5 * when {
                 roll < .1 -> fast()
                 roll < .5 -> medium()
                 roll < .9 -> slow()
                 else -> reallySlow()
+            }).toInt()
+        } else {
+            when {
+                roll < .5 -> fast()
+                roll < .9 -> medium()
+                else -> slow()
             }
         }
     }
@@ -92,7 +108,7 @@ class LatencyGenerator {
 
 @Component
 class HttpStatusGenerator {
-    private val chaos = ChaosState()
+    private val chaos = ChaosState("http-status")
     fun next(): HttpStatus {
         val roll = random.nextFloat()
 
@@ -136,7 +152,20 @@ class RandomEndpoint(
         val latency = latencyGenerator.next()
 
         val response = ResponseEntity.status(status).body("status: $status\n latency: $latency ms")
-        return Mono.just(response).delayElement(Duration.ofMillis(latency.toLong()))
+        return Mono.just(response)
+                .delayElement(Duration.ofMillis(latency.toLong()))
+                .doOnError { e -> println(e) }
+    }
+}
+
+@Component
+class Thingy(val builder: WebClient.Builder) : ApplicationListener<ApplicationReadyEvent> {
+    override fun onApplicationEvent(event: ApplicationReadyEvent) {
+
+        val webClient = builder.build()
+        Flux.interval(Duration.ofMillis(10))
+                .flatMap { webClient.get().uri("http://localhost:8080/random").exchange() }
+                .subscribe()
     }
 }
 
