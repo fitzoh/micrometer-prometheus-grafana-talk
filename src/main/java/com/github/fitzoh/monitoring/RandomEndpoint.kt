c5229a96aa132d0b23993f7751eb5d84e832e46e
Andrew Fitzgerald <andrewcfitzgerald@gmail.com>
2019-03-10 00:35:30 -0500

    set up 2nd instance (with different baseline latency) and add some extra endpoints

--- src/main/java/com/github/fitzoh/monitoring/RandomEndpoint.kt
+++ src/main/java/com/github/fitzoh/monitoring/RandomEndpoint.kt
@@ -2,13 +2,11 @@ package com.github.fitzoh.monitoring
 
 import io.micrometer.core.instrument.MeterRegistry
 import io.micrometer.core.instrument.Metrics
-import net.logstash.logback.argument.StructuredArguments
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.InitializingBean
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.ApplicationListener
-import org.springframework.context.annotation.Bean
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Component
@@ -16,17 +14,13 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Flux
-import reactor.core.publisher.Flux.*
 import reactor.core.publisher.Mono
-import reactor.core.scheduler.Schedulers
 import java.time.Duration
 import java.time.Instant
 import java.util.*
-import java.util.concurrent.CompletableFuture
 import java.util.concurrent.atomic.AtomicLong
 import java.util.concurrent.atomic.AtomicReference
 import java.util.concurrent.locks.ReentrantLock
-import java.util.concurrent.locks.ReentrantReadWriteLock
 
 
 data class ChaosEvent(val start: Instant = Instant.EPOCH, val end: Instant = Instant.EPOCH) {
@@ -80,12 +74,14 @@ class ChaosState(val type: String) {
 }
 
 @Component
-class LatencyGenerator {
+class LatencyGenerator(
+        @Value("\${demo.base-latency}") val baseLatency: Int
+) {
     private val chaos = ChaosState("latency")
     fun next(): Int {
         val roll = random.nextFloat()
 
-        return if (chaos.isEventActive()) {
+        val calculatedLatency = if (chaos.isEventActive()) {
             (200 + 1.5 * when {
                 roll < .1 -> fast()
                 roll < .5 -> medium()
@@ -99,6 +95,8 @@ class LatencyGenerator {
                 else -> slow()
             }
         }
+
+        return baseLatency + calculatedLatency
     }
 
     private fun fast() = random.nextInt(maxMillisFast)
@@ -153,7 +151,7 @@ class RandomEndpoint(
         meterRegistry.gauge("my.random.walk", randomWalk)
     }
 
-    @GetMapping("/random")
+    @GetMapping(value = ["/random", "/also-random", "/yet-another/random"])
     fun endpoint(): Mono<ResponseEntity<String>> {
         if (random.nextBoolean()) randomWalk.incrementAndGet() else randomWalk.decrementAndGet()
 
@@ -168,12 +166,29 @@ class RandomEndpoint(
 }
 
 @Component
-class Thingy(val builder: WebClient.Builder) : ApplicationListener<ApplicationReadyEvent> {
+class Thingy(
+        val builder: WebClient.Builder,
+        @Value("\${demo.client-delay}") val clientDelay: Long,
+        @Value("\${server.port}") val port: String
+) : ApplicationListener<ApplicationReadyEvent> {
+
+    val random = Random()
+
+    fun url(): String {
+        val roll = random.nextFloat()
+
+        return when {
+            roll < .5 -> "http://localhost:$port/random"
+            roll < .85 -> "http://localhost:$port/also-random"
+            else -> "http://localhost:$port/yet-another/random"
+        }
+    }
+
     override fun onApplicationEvent(event: ApplicationReadyEvent) {
 
         val webClient = builder.build()
-        Flux.interval(Duration.ofMillis(10))
-                .flatMap { webClient.get().uri("http://localhost:8080/random").exchange() }
+        Flux.interval(Duration.ofMillis(clientDelay))
+                .flatMap { webClient.get().uri(url()).exchange() }
                 .subscribe()
     }
 }
